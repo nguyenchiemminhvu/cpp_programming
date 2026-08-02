ffc5b96aaca25142d0bfbb812cab84222c352956
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-02 23:23:53 +0700

    cpp11 constexpr keyword

--- study/cpp11/core_language/constexpr/main.cpp
+++ study/cpp11/core_language/constexpr/main.cpp
@@ -1,4 +1,26 @@
+#include <iostream>
+
+constexpr int square(int n) { return n * n; }
+
 int main()
 {
+    // compile-time constant expression
+    constexpr int x = 5;
+    constexpr int y = x * 2;
+    constexpr int z = y + 3;
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+    std::cout << "z: " << z << std::endl;
+
+    // compile-time constant expression with function
+    constexpr int a = square(4);
+    std::cout << "a: " << a << std::endl;
+    std::cout << square(x + y + z) << std::endl;
+
+    int temp = 10;
+    // runtime constant expression
+    int b = square(temp);
+    std::cout << "b: " << b << std::endl;
+
     return 0;
 }
