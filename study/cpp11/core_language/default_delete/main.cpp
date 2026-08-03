6b87de95a8cb8383e85e34720a0dc50667adc39e
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-03 21:00:23 +0900

    cpp11 default and delete keywords

--- study/cpp11/core_language/default_delete/main.cpp
+++ study/cpp11/core_language/default_delete/main.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+
+class base
+{
+public:
+    base() = default;
+    base(const base&) = delete; // delete copy constructor
+    base& operator=(const base&) = delete; // delete copy assignment operator
+
+    virtual void foo() = delete; // delete member function
+    // void bar() = default; // error: default member function
+};
+
+class derived : public base
+{
+public:
+    derived() = default;
+    derived(const derived&) = delete; // delete copy constructor
+    derived& operator=(const derived&) = delete; // delete copy assignment operator
+};
+
 int main()
 {
     return 0;
