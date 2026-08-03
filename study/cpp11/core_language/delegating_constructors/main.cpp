bb1142cd8f256ff4ba0711cb2ce1c5ba3eb4ceac
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-03 20:48:10 +0700

    cpp11 delegating constructors

--- study/cpp11/core_language/delegating_constructors/main.cpp
+++ study/cpp11/core_language/delegating_constructors/main.cpp
@@ -1,1 +1,19 @@
-int main() { return 0; }
+#include <iostream>
+
+class network_config
+{
+public:
+    network_config() : network_config("localhost", 8080) {}
+    network_config(const std::string& host, int port) : host_(host), port_(port) {}
+    network_config(const network_config& other) : network_config(other.host_, other.port_) {}
+    network_config(network_config&& other) : network_config(std::move(other.host_, other.port_)) {}
+
+private:
+    std::string host_;
+    int port_;
+};
+
+int main()
+{
+    return 0;
+}
