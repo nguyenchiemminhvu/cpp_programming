ef424b8f0a5146c40f44051f8afe3d0ac0cb1e5c
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-04 19:25:28 +0900

    cpp11 range based for

--- study/cpp11/core_language/range_based_for/main.cpp
+++ study/cpp11/core_language/range_based_for/main.cpp
@@ -1,4 +1,26 @@
+#include <iostream>
+#include <vector>
+
 int main()
 {
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+
+    for (int value : vec)
+    {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+
+    for (auto& value : vec)
+    {
+        value *= 2; // Modify the elements in the vector
+    }
+
+    for (int value : vec)
+    {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
