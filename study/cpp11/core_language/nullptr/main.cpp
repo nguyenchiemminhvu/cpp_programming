0078b219377b7eb82fa066a38e69eb34b5461885
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-04 19:12:55 +0900

    cpp11 nullptr

--- study/cpp11/core_language/nullptr/main.cpp
+++ study/cpp11/core_language/nullptr/main.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+
+void foo(int x)
+{
+    std::cout << "foo(int): " << x << std::endl;
+}
+
+void foo(char* x)
+{
+    std::cout << "foo(char*): " << x << std::endl;
+}
+
 int main()
 {
+    foo(NULL); // Calls foo(int) because NULL is typically defined as 0
+    foo(nullptr); // Calls foo(char*) because nullptr is a null pointer constant
     return 0;
 }
