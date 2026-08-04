59e4f65107e6f6be1ed0e152becc275acdc00433
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-04 14:53:38 +0900

    cpp11 inheriting constructors

--- study/cpp11/core_language/inheriting_constructors/main.cpp
+++ study/cpp11/core_language/inheriting_constructors/main.cpp
@@ -1,1 +1,26 @@
-int main() { return 0; }
+#include <iostream>
+
+class base
+{
+public:
+    base(int x) {}
+    base(int x, double y) {}
+    base(std::string name) {}
+};
+
+class derived : public base
+{
+public:
+    using base::base; // Inherit constructors from base class
+
+    int extra; // danger, this member is not initialized by the inherited constructors
+    int default_extra = 0; // This member will be initialized to 0 by default
+};
+
+int main()
+{
+    derived d1(10); // Calls base(int x)
+    derived d2(10, 3.14); // Calls base(int x, double y)
+    derived d3("Hello"); // Calls base(std::string name)
+    return 0;
+}
