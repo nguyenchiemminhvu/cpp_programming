1d8f0cf6b4963fb44ce3662329b72f3586edbfa8
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-08 14:43:24 +0700

    cpp11 reference collapsing

--- study/cpp11/utilities/reference_collapsing/main.cpp
+++ study/cpp11/utilities/reference_collapsing/main.cpp
@@ -1,1 +1,31 @@
-int main() { return 0; }
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+template <typename T>
+void inspect_reference(T&& arg)
+{
+    if (std::is_lvalue_reference<decltype(arg)>::value)
+    {
+        std::cout << "arg is an lvalue reference." << std::endl;
+    }
+    else if (std::is_rvalue_reference<decltype(arg)>::value)
+    {
+        std::cout << "arg is an rvalue reference." << std::endl;
+    }
+    else
+    {
+        std::cout << "arg is neither an lvalue nor an rvalue reference." << std::endl;
+    }
+}
+
+int main()
+{
+    int val = 42;
+    inspect_reference(val);          // lvalue
+    inspect_reference(*&val);        // lvalue
+    inspect_reference(std::move(val)); // rvalue
+    inspect_reference(42);           // rvalue
+
+    return 0;
+}
