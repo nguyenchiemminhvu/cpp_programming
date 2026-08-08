db6421a2bb5dc0ae4c5ebccee453507e86fd0e85
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-08 15:43:11 +0700

    cpp11 type_traits

--- study/cpp11/utilities/type_traits/main.cpp
+++ study/cpp11/utilities/type_traits/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+void analyzeAndSanitize()
+{
+    std::cout << "Is pointer? " << std::is_pointer<T>::value << "\n";
+    std::cout << "Is const? "   << std::is_const<T>::value << "\n";
+    using StrippedType = typename std::remove_pointer<T>::type;
+    std::cout << "Is stripped type const? " << std::is_const<StrippedType>::value << "\n";
+}
+
 int main()
 {
+    analyzeAndSanitize<const int*>();
     return 0;
 }
