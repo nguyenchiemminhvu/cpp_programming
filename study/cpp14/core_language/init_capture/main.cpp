8529bfaabf5949e6660f958fd8971ba321fc13f4
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-09 22:59:07 +0700

    cpp14 lambda init captures

--- study/cpp14/core_language/init_capture/main.cpp
+++ study/cpp14/core_language/init_capture/main.cpp
@@ -1,1 +1,36 @@
-int main() { return 0; }
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    auto ptr = std::make_unique<int>(42);
+    auto f = [p = std::move(ptr)]() {
+        std::cout << *p << std::endl;
+    };
+    f();
+
+    if (ptr)
+    {
+        std::cout << "ptr is not null" << std::endl;
+    }
+    else
+    {
+        std::cout << "ptr is null" << std::endl;
+    }
+
+    int factor = 2;
+    auto f_double = [mul = factor](int x) {
+        return x * mul;
+    };
+
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    std::transform(numbers.begin(), numbers.end(), numbers.begin(), f_double);
+    for (const auto& num : numbers)
+    {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
