dafa398d0f39782d9b8932dee3d40973adb68438
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-11 19:04:18 +0900

    cpp14 make unique ptr

--- study/cpp14/standard_library/make_unique/main.cpp
+++ study/cpp14/standard_library/make_unique/main.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
+#include <memory>
+
 int main()
 {
+    std::unique_ptr<int> ptr = std::make_unique<int>(42);
+    std::cout << "Value: " << *ptr << std::endl;
+
     return 0;
 }
