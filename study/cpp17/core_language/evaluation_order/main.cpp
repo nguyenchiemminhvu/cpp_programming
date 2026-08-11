998a6fb8fbb4b7bd1de7cb1d7e9d58bfd1230473
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-11 21:16:31 +0900

    cpp17 evaluation orders

--- study/cpp17/core_language/evaluation_order/main.cpp
+++ study/cpp17/core_language/evaluation_order/main.cpp
@@ -1,1 +1,22 @@
-int main() { return 0; }
+#include <iostream>
+#include <string>
+
+int val = 0;
+
+int f()
+{
+    return ++val;
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    std::cout << add(f(), f()) << std::endl;
+    std::cout << f() << " " << f() << std::endl;
+
+    return 0;
+}
