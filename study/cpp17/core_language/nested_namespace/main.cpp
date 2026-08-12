a174de43e6d680467dad8e0db3cfff3316d441e8
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-12 17:11:27 +0900

    cpp17 nested namespaces

--- study/cpp17/core_language/nested_namespace/main.cpp
+++ study/cpp17/core_language/nested_namespace/main.cpp
@@ -1,1 +1,34 @@
-int main() { return 0; }
+#include <iostream>
+
+namespace lge::chassis::engine
+{
+    void start()
+    {
+        std::cout << "Engine started" << std::endl;
+    }
+}
+
+namespace lge::chassis::transmission
+{
+    void shift()
+    {
+        std::cout << "Transmission shifted" << std::endl;
+    }
+}
+
+namespace bosch::sensors
+{
+    void calibrate()
+    {
+        std::cout << "Sensors calibrated" << std::endl;
+    }
+}
+
+int main()
+{
+    lge::chassis::engine::start();
+    lge::chassis::transmission::shift();
+    bosch::sensors::calibrate();
+
+    return 0;
+}
