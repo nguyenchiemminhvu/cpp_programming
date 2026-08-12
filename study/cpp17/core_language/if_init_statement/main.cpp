3e3d76d5d530547a55e15a74a912da086906575e
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-12 14:28:05 +0900

    cpp17 if init statement

--- study/cpp17/core_language/if_init_statement/main.cpp
+++ study/cpp17/core_language/if_init_statement/main.cpp
@@ -1,4 +1,34 @@
+#include <iostream>
+#include <mutex>
+
+std::mutex mtx;
+
+auto get_status()
+{
+    std::lock_guard<std::mutex> lock(mtx);
+    return 1;
+}
+
 int main()
 {
+    if (auto status = get_status(); status == 1)
+    {
+        std::cout << "Status is 1" << std::endl;
+    }
+    else
+    {
+        std::cout << "Status is not 1" << std::endl;
+    }
+
+    int status = get_status();
+    if (std::lock_guard<std::mutex> lock(mtx); status == 1)
+    {
+        std::cout << "Status is 1" << std::endl;
+    }
+    else
+    {
+        std::cout << "Status is not 1" << std::endl;
+    }
+
     return 0;
 }
