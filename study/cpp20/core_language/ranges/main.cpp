1da444d9bfbd209b8061549563cfb6184f32781e
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-15 15:43:28 +0700

    cpp20 ranges

--- study/cpp20/core_language/ranges/main.cpp
+++ study/cpp20/core_language/ranges/main.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+#include <type_traits>
+#include <vector>
+#include <ranges>
+
 int main()
 {
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto res = vec | std::views::transform([](int val) { return val * 2; });
+
+    for (auto val : res)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
