e0886a18a3f940fd46bf88f1a1753a614fad3579
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-16 15:03:34 +0700

    cpp20 bit cast

--- study/cpp20/standard_library/bit_cast/main.cpp
+++ study/cpp20/standard_library/bit_cast/main.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+#include <bit>
+
 int main()
 {
+    float f = 3.14f;
+
+    // Use std::bit_cast to reinterpret the bits of the float as an integer
+    uint32_t i = std::bit_cast<uint32_t>(f);
+    std::cout << "Float: " << f << ", Bit-cast to uint32_t: " << i << std::endl;
+
+    f = std::bit_cast<float>(i);
+    std::cout << "Bit-cast back to float: " << f << std::endl;
+
     return 0;
 }
