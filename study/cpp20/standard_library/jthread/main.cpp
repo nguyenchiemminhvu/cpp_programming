2823eff5ac402f3b25dced19d55317b491807709
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-16 23:51:05 +0700

    cpp20 jthread

--- study/cpp20/standard_library/jthread/main.cpp
+++ study/cpp20/standard_library/jthread/main.cpp
@@ -1,4 +1,35 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void worker_thread(std::stop_token stoken)
+{
+    while (!stoken.stop_requested())
+    {
+        std::cout << "Worker thread is running..." << std::endl;
+
+        for (int i = 0; i < 5; ++i)
+        {
+            if (stoken.stop_requested())
+            {
+                std::cout << "Worker thread received stop request." << std::endl;
+                break;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+
+    std::cout << "Worker thread is stopping..." << std::endl;
+}
+
 int main()
 {
+    {
+        std::jthread jt(worker_thread);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    std::cout << "Main thread is exiting..." << std::endl;
+
     return 0;
 }
