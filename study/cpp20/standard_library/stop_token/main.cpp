291656d7cb1cb20a64a4fc02ed0049d108031a9b
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-16 23:54:21 +0700

    cpp20 stop token

--- study/cpp20/standard_library/stop_token/main.cpp
+++ study/cpp20/standard_library/stop_token/main.cpp
@@ -1,1 +1,22 @@
-int main() { return 0; }
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <stop_token>
+
+int main()
+{
+    std::stop_source stop_signal;
+
+    std::jthread worker{[token = stop_signal.get_token()]() {
+        while (!token.stop_requested()) {
+            std::cout << "Working..." << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+        std::cout << "Stop requested. Exiting thread." << std::endl;
+    }};
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    stop_signal.request_stop();
+
+    return 0;
+}
