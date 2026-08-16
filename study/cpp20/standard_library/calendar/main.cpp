0d3a648cd772b35772aa0ccfad37275568d058c3
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-16 18:56:02 +0700

    cpp20 calendar

--- study/cpp20/standard_library/calendar/main.cpp
+++ study/cpp20/standard_library/calendar/main.cpp
@@ -1,4 +1,20 @@
+#include <chrono>
+#include <iostream>
+
 int main()
 {
+    using namespace std::chrono;
+
+    const year_month_day date = 2026y / August / 16;
+
+    std::cout << "Year : " << int(date.year()) << '\n';
+    std::cout << "Month: " << unsigned(date.month()) << '\n';
+    std::cout << "Day  : " << unsigned(date.day()) << '\n';
+
+    std::cout << "Valid: "
+              << std::boolalpha
+              << date.ok()
+              << '\n';
+
     return 0;
 }
