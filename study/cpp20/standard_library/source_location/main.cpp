baa372568c3db496a6b0dca05e2745db6c485803
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-16 21:58:11 +0700

    cpp20 source location

--- study/cpp20/standard_library/source_location/main.cpp
+++ study/cpp20/standard_library/source_location/main.cpp
@@ -1,4 +1,38 @@
+#include <iostream>
+#include <source_location>
+#include <utility>
+
+template <typename... Args>
+void print_impl(
+    const std::source_location& location,
+    Args&&... args)
+{
+    std::cout
+        << "File: " << location.file_name()
+        << ", Line: " << location.line()
+        << ", Function: " << location.function_name()
+        << '\n';
+
+    (std::cout << ... << std::forward<Args>(args)) << '\n';
+}
+
+#define PRINT(...) \
+    print_impl(std::source_location::current(), __VA_ARGS__)
+
+void foo()
+{
+    PRINT("Hello from foo!");
+}
+
+void bar()
+{
+    PRINT("Hello from bar!");
+}
+
 int main()
 {
+    foo();
+    bar();
+
     return 0;
 }
