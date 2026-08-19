5ba7227a49368f01fad81c9546a1dd2d2f8732c5
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-19 13:31:37 +0900

    cpp11 type traits and decay #2

--- study/cpp11/utilities/type_traits/main.cpp
+++ study/cpp11/utilities/type_traits/main.cpp
@@ -2,6 +2,51 @@
 #include <type_traits>
 
 template <typename T>
+struct custom_decay
+{
+    using type = typename custom_decay<typename std::decay<T>::type>::type;
+};
+
+template <>
+struct custom_decay<char*>
+{
+    using type = std::string;
+};
+
+template <>
+struct custom_decay<const char*>
+{
+    using type = std::string;
+};
+
+template <size_t N>
+struct custom_decay<char[N]>
+{
+    using type = std::string;
+};
+
+template <size_t N>
+struct custom_decay<const char[N]>
+{
+    using type = std::string;
+};
+
+template <typename T>
+using custom_decay_t = typename custom_decay<T>::type;
+
+template <typename T>
+custom_decay_t<T> build_custom_decay(T&& t)
+{
+    return custom_decay_t<T>(std::forward<T>(t));
+}
+
+template <typename T, typename U>
+inline std::pair<typename std::decay<T>::type, typename std::decay<U>::type> build_pair(T&& t, U&& u)
+{
+    return std::pair<typename std::decay<T>::type, typename std::decay<U>::type>(std::forward<T>(t), std::forward<U>(u));
+}
+
+template <typename T>
 void analyzeAndSanitize()
 {
     std::cout << "Is pointer? " << std::is_pointer<T>::value << "\n";
@@ -32,12 +77,6 @@ void print_decay_type(T&& arg)
     }
 }
 
-template <typename T, typename U>
-inline std::pair<typename std::decay<T>::type, typename std::decay<U>::type> build_pair(T&& t, U&& u)
-{
-    return std::pair<typename std::decay<T>::type, typename std::decay<U>::type>(std::forward<T>(t), std::forward<U>(u));
-}
-
 int main()
 {
     analyzeAndSanitize<const int*>();
@@ -47,5 +86,10 @@ int main()
 
     auto p = build_pair(42, new int(42));
     std::cout << "Pair first: " << p.first << ", Pair second: "<< *p.second << "\n";
+
+    auto cd = build_custom_decay("Hello, World!");
+    static_assert(std::is_same<decltype(cd), std::string>::value, "Custom decay failed");
+    std::cout << "Custom decayed value: " << cd.c_str() << "\n";
+
     return 0;
 }
