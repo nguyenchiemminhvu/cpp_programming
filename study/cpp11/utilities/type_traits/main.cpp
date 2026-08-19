a2bebf0f771a050e6a7b0d9d616c98a9c2bebae6
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-19 13:20:45 +0900

    cpp11 type traits and decay

--- study/cpp11/utilities/type_traits/main.cpp
+++ study/cpp11/utilities/type_traits/main.cpp
@@ -10,8 +10,42 @@ void analyzeAndSanitize()
     std::cout << "Is stripped type const? " << std::is_const<StrippedType>::value << "\n";
 }
 
+template <typename T>
+void print_decay_type(T&& arg)
+{
+    using decay_type = typename std::decay<T>::type;
+    if (std::is_same<decay_type, int>::value)
+    {
+        std::cout << "The decayed type is int.\n";
+    }
+    else if (std::is_same<decay_type, int*>::value)
+    {
+        std::cout << "The decayed type is int*.\n";
+    }
+    else if (std::is_same<decay_type, void(*)()>::value)
+    {
+        std::cout << "The decayed type is void(*)().\n";
+    }
+    else
+    {
+        std::cout << "The decayed type is unknown.\n";
+    }
+}
+
+template <typename T, typename U>
+inline std::pair<typename std::decay<T>::type, typename std::decay<U>::type> build_pair(T&& t, U&& u)
+{
+    return std::pair<typename std::decay<T>::type, typename std::decay<U>::type>(std::forward<T>(t), std::forward<U>(u));
+}
+
 int main()
 {
     analyzeAndSanitize<const int*>();
+    print_decay_type(42);
+    print_decay_type(new int(42));
+    print_decay_type([]() { std::cout << "Hello, World!\n"; });
+
+    auto p = build_pair(42, new int(42));
+    std::cout << "Pair first: " << p.first << ", Pair second: "<< *p.second << "\n";
     return 0;
 }
