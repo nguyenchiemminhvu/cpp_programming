7fedea40705f2dc911b2e9778d529d2dca0c97b3
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-23 00:46:24 +0700

    cpp programming pattern crtp update

--- study/patterns/CRTP/main.cpp
+++ study/patterns/CRTP/main.cpp
@@ -22,6 +22,35 @@ public:
 };
 
 template <typename Derived>
+class addable
+{
+public:
+    Derived operator+(const Derived &other) const
+    {
+        return static_cast<const Derived *>(this)->add(other);
+    }
+};
+
+class vector2d : public addable<vector2d>
+{
+public:
+    vector2d(float x, float y) : x_(x), y_(y) {}
+
+    vector2d add(const vector2d &other) const
+    {
+        return vector2d(x_ + other.x_, y_ + other.y_);
+    }
+
+    void print() const
+    {
+        std::cout << "vector2d(" << x_ << ", " << y_ << ")" << std::endl;
+    }
+private:
+    float x_;
+    float y_;
+};
+
+template <typename Derived>
 class instance_counter
 {
 public:
@@ -61,6 +90,11 @@ public:
 
 int main()
 {
+    vector2d v1(1.0f, 2.0f);
+    vector2d v2(3.0f, 4.0f);
+    vector2d v3 = v1 + v2;
+    v3.print();
+
     engine1 e1_1, e1_2;
     {
         engine2 e2_1, e2_2, e2_3;
