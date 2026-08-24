a7b5e8ff5828594e8997e81a5f971715fffa5666
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-24 23:24:12 +0700

    castle bit mask operations

--- ncmv_projects/castle/bit/bit_core.h
+++ ncmv_projects/castle/bit/bit_core.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <type_traits>
 #include <cstdint>
 #include <climits>
 
@@ -9,52 +10,68 @@ namespace bit
 {
 
 template <typename T>
-constexpr bool test(T value, uint8_t bit_index)
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, bool>::type
+test(T value, uint8_t bit_index)
 {
     return (value >> bit_index) & T{1U} != T{0U};
 }
 
 template <typename T, std::size_t bit_index>
-constexpr bool test(T value)
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, bool>::type
+test(T value)
 {
     static_assert(bit_index < sizeof(T) * CHAR_BIT, "bit_index is out of range for the type T");
     return (value >> bit_index) & T{1U} != T{0U};
 }
 
 template <typename T>
-constexpr T set(T value, uint8_t bit_index)
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+set(T value, uint8_t bit_index)
 {
     return static_cast<T>(value | (T{1U} << bit_index));
 }
 
 template <typename T, std::size_t bit_index>
-constexpr T set(T value)
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+set(T value)
 {
     static_assert(bit_index < sizeof(T) * CHAR_BIT, "bit_index is out of range for the type T");
     return static_cast<T>(value | (T{1U} << bit_index));
 }
 
 template <typename T>
-constexpr T clear(T value, uint8_t bit_index)
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+clear(T value, uint8_t bit_index)
 {
     return static_cast<T>(value & static_cast<T>(~(T{1U} << bit_index)));
 }
 
 template <typename T, std::size_t bit_index>
-constexpr T clear(T value)
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+clear(T value)
 {
     static_assert(bit_index < sizeof(T) * CHAR_BIT, "bit_index is out of range for the type T");
     return static_cast<T>(value & static_cast<T>(~(T{1U} << bit_index)));
 }
 
 template <typename T>
-constexpr T toggle(T value, uint8_t bit_index)
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+toggle(T value, uint8_t bit_index)
 {
     return static_cast<T>(value ^ (T{1U} << bit_index));
 }
 
 template <typename T, std::size_t bit_index>
-constexpr T toggle(T value)
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+toggle(T value)
 {
     static_assert(bit_index < sizeof(T) * CHAR_BIT, "bit_index is out of range for the type T");
     return static_cast<T>(value ^ (T{1U} << bit_index));
--- ncmv_projects/castle/bit/bit_mask.h
+++ ncmv_projects/castle/bit/bit_mask.h
@@ -1,1 +1,88 @@
 #pragma once
+
+#include <type_traits>
+#include <cstdint>
+
+namespace castle
+{
+namespace bit
+{
+
+template <typename T>
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+all_bits_mask()
+{
+    return static_cast<T>(~T{0U});
+}
+
+template <typename T>
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+single_bit_mask(uint8_t bit_index)
+{
+    return static_cast<T>(T{1U} << bit_index);
+}
+
+template <typename T, std::size_t bit_index>
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+single_bit_mask()
+{
+    static_assert(bit_index < sizeof(T) * CHAR_BIT, "bit_index is out of range for the type T");
+    return static_cast<T>(T{1U} << bit_index);
+}
+
+template <typename T>
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+low_bits_mask(uint8_t bit_count)
+{
+    return static_cast<T>(T{1U} << bit_count) - T{1U};
+}
+
+template <typename T, std::size_t bit_count>
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+low_bits_mask()
+{
+    static_assert(bit_count <= sizeof(T) * CHAR_BIT, "bit_count is out of range for the type T");
+    return static_cast<T>(T{1U} << bit_count) - T{1U};
+}
+
+template <typename T>
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+high_bits_mask(uint8_t bit_count)
+{
+    return static_cast<T>(~(T{1U} << (sizeof(T) * CHAR_BIT - bit_count)) + T{1U});
+}
+
+template <typename T, std::size_t bit_count>
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+high_bits_mask()
+{
+    static_assert(bit_count <= sizeof(T) * CHAR_BIT, "bit_count is out of range for the type T");
+    return static_cast<T>(~(T{1U} << (sizeof(T) * CHAR_BIT - bit_count)) + T{1U});
+}
+
+template <typename T>
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+range_mask(uint8_t start_bit_index, uint8_t bit_count)
+{
+    return static_cast<T>(low_bits_mask<T>(bit_count) << start_bit_index);
+}
+
+template <typename T, std::size_t start_bit_index, std::size_t bit_count>
+constexpr
+typename std::enable_if_t<std::is_integral<T>::value, T>::type
+range_mask()
+{
+    static_assert(start_bit_index + bit_count <= sizeof(T) * CHAR_BIT, "start_bit_index + bit_count is out of range for the type T");
+    return static_cast<T>(low_bits_mask<T>(bit_count) << start_bit_index);
+}
+
+} // namespace bit
+} // namespace castle
