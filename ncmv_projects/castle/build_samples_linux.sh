addc8c1b29e7425b849eb0fc1c171398d0d6b8ad
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2026-08-29 16:03:51 +0700

    move castle to external git repo

--- ncmv_projects/castle/build_samples_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf build/*
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCASTLE_BUILD_SAMPLES=ON
-cmake --build build --parallel
--- ncmv_projects/castle/build_test_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf build/*
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCASTLE_BUILD_TESTING=ON -DCASTLE_FETCH_GTEST=ON
-cmake --build build --parallel
